4dd89575b6312a80524fa8710f9142045bfd0265
YakumaKuramy <tsuoygames@gmail.com>
2026-03-10 00:56:24 -0300

    build: Addin new folders

---
+++ programacao-algoritmos-python/02-operators/6_cone.py
@@ -0,0 +1,6 @@
+# Let's ask the user to enter the value of the radius and height of cone ray 
+
+radius = int(input("Enter the value of the cone radius: "))
+height = int(input("Infuse the cone height value: "))
+
+
